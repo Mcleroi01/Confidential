48ab0b17a949b231673e53cc95593b313701e6c4
Musongela <142029238+Mcleroi01@users.noreply.github.com>
2023-09-20 15:03:45 +0100

    apropos

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,23 @@ function showPage() {
   document.getElementById("footer").style.display = "block";
   document.getElementById("mob").style.display = "block";
 }
+
+window.addEventListener('scroll',reveal)
+function reveal() {
+	var reveals=document.querySelectorAll('.reveal');
+
+	for (let i = 0; i < reveals.length; i++) {
+		let windowheight = window.innerHeight;
+		let revealtop= reveals[i].getBoundingClientRect().top
+		let revealpoint= 150;
+
+
+		if(revealtop < windowheight - revealpoint){
+			reveals[i].classList.add('active');
+
+		}else{
+			reveals[i].classList.remove('active');
+		}
+		
+	}
+}
